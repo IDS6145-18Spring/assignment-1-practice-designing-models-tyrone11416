e710909aeb8cc019e8e9849a25af04fccb5ce354
tyrone11416 <34398477+tyrone11416@users.noreply.github.com>
2018-01-17 09:55:34 -0500

    ssl code update

---
+++ code/SSL code/Enviromental Sensors.py
@@ -0,0 +1,10 @@
+class enviromental sensors():
+    '''This is the enviromental sensor'''
+
+        def __init__(self, n, aq, g, t, cg, s):
+            self.name = n
+            self.air quality = aq
+            self.gas = g
+            self.toxins = t
+            self.chemical gas = cg
+            self.seismic activity = s
---
+++ code/SSL code/Wireless Transceiver.py
@@ -0,0 +1,8 @@
+class Wireless Tranceiver():
+    '''This is the Wireless Tranceiver'''
+
+    def __init__(self, n, w, us, ds):
+            self.name = n
+            self.wifi signal = w
+            self.upload speed = us
+            self.download speed = ds
