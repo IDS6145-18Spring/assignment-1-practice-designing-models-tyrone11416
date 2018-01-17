491c87a6492d14efda4411a0f46533f37b74e474
tyrone11416 <34398477+tyrone11416@users.noreply.github.com>
2018-01-17 11:09:02 -0500

    Main file update with new folder SSL CODE

--- code/Water_Detection.py
+++
@@ -1,8 +0,0 @@
-class water detection():
-    '''This is the water detection station'''
-
-        def __init__(self, n, p, h):
-            self.name = n
-            self.percipitation = p
-            self.humidity = h
-           
--- code/Wireless_Transceiver.py
+++
@@ -1,8 +0,0 @@
-class Wireless Tranceiver():
-    '''This is the Wireless Tranceiver'''
-
-    def __init__(self, n, w, us, ds):
-            self.name = n
-            self.wifi signal = w
-            self.upload speed = us
-            self.download speed = ds
